e363bd11e3f6df8011232805bbe542e5be5c51f0
Mark-James McDougall <contact@markjames.dev>
2025-04-14 18:59:15 -0400

    Add RomLoader.cs with functionality to load a ROM from a pre-determined list.

---
+++ src/Helpers/RomLoader.cs
@@ -0,0 +1,52 @@
+namespace Chippy.Helpers;
+
+public static class RomLoader
+{
+    public static string Load()
+    {
+        var romDir = GetRomDirectory();
+        var romPath = GetRomPath(romDir);
+
+        if (!File.Exists(romPath))
+        {
+            Console.WriteLine("Error: Failed to load rom. Check if the rom");
+        }
+        
+        return romPath;
+    }
+    
+    private static string GetRomDirectory()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var romsDirectory = Path.Join(currentDirectory, "/roms/");
+
+        return romsDirectory;
+    }
+
+    private static string GetRomPath(string romDirectory)
+    {
+        while (true)
+        {
+            Console.WriteLine("Select ROM to load:\n");
+            Console.WriteLine("1 - CHIP-8 Splash Screen");
+            Console.WriteLine("2 - Exit Chippy");
+            Console.Write("Enter your choice: ");
+
+            var romChoice = Console.ReadLine();
+
+            switch (romChoice)
+            {
+                case "1":
+                    return romDirectory + "IBM-Logo.ch8";
+                case "2":
+                    Console.WriteLine("Exiting Chippy...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Try again.");
+                    Console.WriteLine("");
+                    break;
+            }
+        }
+    }
+}
