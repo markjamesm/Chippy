23800b30978616786fea89767310f28ab6181abd
Mark-James McDougall <contact@markjames.dev>
2025-04-14 19:45:08 -0400

    Implement FetchOpcode method.

--- src/Emulator.cs
+++ src/Emulator.cs
@@ -10,8 +10,8 @@ public class Emulator
     // Address register
     private ushort _i = 0;
 
-    // Program counter
-    private int pc = 0;
+    // Program counter, start at program memory.
+    private int _pc = 0x200;
     
     private bool[,] _frameBuffer = new bool[32, 64];
     
@@ -20,5 +20,13 @@ public class Emulator
         var romStream = File.ReadAllBytes(romPath);
         romStream.CopyTo(_memory, 0x200);
         FontSet.Fonts.CopyTo(_memory, 0);
+    }
+
+    public uint FetchOpcode()
+    {
+        var opcode = (uint)_memory[_pc] << 8 | _memory[_pc + 1];
+        _pc++;
+        
+        return opcode;
     }
 }
