c46f7320a540d192cbdb7e4f9b1b5633d533340b
Mark-James McDougall <contact@markjames.dev>
2025-04-14 21:24:49 -0400

    Add DXYN instruction.

--- src/Emulator.cs
+++ src/Emulator.cs
@@ -12,10 +12,10 @@ public class Emulator
 
     // Program counter, start at program memory.
     private int _pc = 0x200;
-    private bool[,] _frameBuffer = new bool[32, 64];
-    
     private const int ProgramStart = 0x200;
     private const int FontStart = 0;
+    
+    public bool[,] FrameBuffer { get; private set; } = new bool[32, 64];
     
     public Emulator(string romPath)
     {
@@ -57,6 +57,10 @@ public class Emulator
             case 0xA000:
                 ExecuteAnnn(nnn);
                 break;
+            case 0xD000:
+                ExecuteDxyn(x, y, n);
+                break;
+                
         }
     }
 
@@ -72,7 +76,7 @@ public class Emulator
 
     private void Execute00E0()
     {
-        _frameBuffer = new bool[32, 64];
+        FrameBuffer = new bool[32, 64];
     }
 
     private void Execute6Xnn(byte x, byte nn)
@@ -83,5 +87,52 @@ public class Emulator
     private void ExecuteAnnn(ushort nnn)
     {
         _i = nnn;
+    }
+
+    private void ExecuteDxyn(byte x, byte y, byte n)
+    {
+        // Modulo to wrap coords rather than clip on screen
+        var xCoord = _v[x] % 64;
+        var yCoord = _v[y] % 32;
+
+        _v[0xF] = 0;
+        
+        for (var row = 0; row < n; row++)
+        {
+            if (row + yCoord >= 32)
+            {
+                break;
+            }
+            
+            var spriteByte = _memory[_i + row];
+
+            // CHIP-8 sprites are always a byte, so we can always
+            // assume 8 bits per row.
+            for (var col = 0; col < 8; col++)
+            {
+                if (col + xCoord >= 64)
+                {
+                    break;
+                }
+
+                var currentPixel = (spriteByte >> col) != 0;
+
+                if (!currentPixel)
+                {
+                    continue;
+                }
+                
+                var screenPixel = FrameBuffer[yCoord + row, xCoord + col];
+
+                if (currentPixel && screenPixel)
+                {
+                    _v[0xF] = 1;
+                }
+
+                // XOR does the same thing as 
+                // Framebuffer[xCoord + col, yCoord + row] = !Framebuffer[xCoord + col, yCoord + row];
+                FrameBuffer[yCoord + row, xCoord + col] ^= true;
+            }
+        }
     }
 }
