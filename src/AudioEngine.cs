b5b5f452d6f0a0d7be9076f48132ae1d9af5b15b
Mark-James McDougall <contact@markjames.dev>
2025-04-26 23:26:25 -0400

    Add Beep.wav
    Implement AudioEngine.cs

---
+++ src/AudioEngine.cs
@@ -0,0 +1,19 @@
+using Raylib_cs;
+
+namespace Chippy;
+
+public class AudioEngine
+{
+    private readonly Sound _beep;
+    
+    public AudioEngine()
+    {
+        Raylib.InitAudioDevice();
+        _beep = Raylib.LoadSound("sounds/beep.wav");
+    }
+
+    public void Beep()
+    {
+        Raylib.PlaySound(_beep);
+    }
+}
